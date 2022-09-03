b8093bef27b38513cf9b664c2a2c4e932d3363c5
Magmastorm3007 <81619774+Magmastorm3007@users.noreply.github.com>
2022-09-03 20:33:59 +0530

    Create Nexxt_permutation.cpp

---
+++ Nexxt_permutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& v) {
+       int n=v.size();
+        int i,j;
+        for(i=n-2;i>=0;i--){
+            if(v[i]<v[i+1])
+                break;
+        }
+        if(i<0)
+            reverse(v.begin(),v.end());
+        else{
+            for(j=n-1;j>=0;j--)
+                if(v[j]>v[i])
+                    break;
+        
+        swap(v[i],v[j]);
+        reverse(v.begin()+i+1,v.end());
+        }
+    }
+    
+};
